15c70c7335a5e16163adb130bf042ad7aa43807c
vigneshj04 <vigneshjmax@gmail.com>
2022-05-06 08:36:14 +0530

    Added: Database Connection To Class

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,2 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static String url = "jdbc:mysql://localhost:3306/bridgelabz";
+    private static String dbUserName = "root";
+    private static String dbPass = "Parrot@22";
+    public static Connection getConnection(){
+        try{
+            //1. load driver class
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //2.connection
+            Connection con = DriverManager.getConnection(url, dbUserName, dbPass);
+            return con;
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- src/EmployeePayroll.java
+++ src/EmployeePayroll.java
@@ -1,2 +1,18 @@
-public class EmployeePayroll {
+
+import java.sql.*;
+
+public class EmployeePayroll{
+
+    public static void main(String[] args) {
+        try {
+            Connection con = DBConnection.getConnection();
+            Statement stmt = con.createStatement();
+            stmt.close();
+            con.close();
+
+            System.out.println("Done!!!");
+        }catch (SQLException | NullPointerException e) {
+            System.out.println(e);
+        }
+    }
 }
